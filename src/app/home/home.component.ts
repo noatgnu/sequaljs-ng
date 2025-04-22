fca9c4e2686268fb1974565dd32c2654edb84084
Toan Phung <toan.phung@proteo.info>
2025-04-22 12:09:55 +0100

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
 import {RouterLink} from '@angular/router';
-
+import {Sequence} from 'sequaljs/dist/sequence';
 @Component({
   selector: 'app-home',
   imports: [
