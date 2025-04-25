e68a944d423445a4df8f5abf7ae2c6919c23dcf0
Toan Phung <toan.phung@proteo.info>
2025-04-25 20:48:35 +0100

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import {MatIcon} from '@angular/material/icon';
 import {RouterLink} from '@angular/router';
 import {Sequence} from 'sequaljs/dist/sequence';
+import {MatCard, MatCardContent} from '@angular/material/card';
 @Component({
   selector: 'app-home',
   imports: [
     MatIcon,
-    RouterLink
+    RouterLink,
+    MatCard,
+    MatCardContent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
